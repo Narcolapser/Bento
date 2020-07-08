4fdf85ab5325bba10bf9502cbf9a178ce74d5fa7
Toben Archer <sandslash@gmail.com>
2020-07-07 21:39:06 -0500

    Setting up react.
    I vascilated on this a lot. Setting up Bento with react means that
    people may have to setup NPM on their machine to get Bento to work. That
    meakes this project a little less approachable. But I think if I play my
    cards right I can still keep it easy to setup Bento.
    
    But there were two things that finally convinced me:
    1. The UI is still seperated from the Backend. This means that the idea
    of building apps and scripting interfaces is not harmed by using React
    to do the default web-based interface.
    2. One of my primary objectives is minimal effort on my part. That
    objective ranks higher than ease of adoption, at least at this point. So
    I want to simply say "npm install jsdiff" to get my differencing engine
    working for example, not trying to hack it into place.
    
    So React is in. Next time I'm on I'll hopefully get diffs working and
    then I can move on to the creature comforts like a decent file browser.

--- bento-ui/src/App.js
+++ bento-ui/src/App.js
@@ -24,6 +24,7 @@ function App() {
 			<textarea id="editor" name="editor" rows="10" cols="50"></textarea>
 			<button onClick={postData}>Save</button>
 			<br/>
+			<p>My Token = {window.token}</p>
 		</div>
 	);
 }
